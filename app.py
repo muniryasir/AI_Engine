e56ecfef0b8b9a1ef293690252d191634c14341a
muniryasir <muniryasir2009@gmail.com>
2023-04-10 07:42:47 +0500

    modified:   __pycache__/app.cpython-36.pyc 	modified:   app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
+# python3 -m flask run
+import simfin as sf
+from simfin.names import *
+
+sf.set_api_key('a3dde4ef-6674-4b65-84a3-a9ff2e7c81f2')
+sf.set_data_dir('~/simfin_data/')
+df = sf.load_income(variant='annual', market='us')
+# @app.route("/")
+# def home():
+#     return "Hello World! I'm using Flask."
 
 @app.route("/")
-def home():
-    return "Hello World! I'm using Flask."
-
-@app.route("/stockdata")
-def home():
-    return "meow"
+def stockdata():
+    print(df.loc['MSFT', [REVENUE, NET_INCOME]])
+    return df.loc['MSFT', [REVENUE, NET_INCOME]]
