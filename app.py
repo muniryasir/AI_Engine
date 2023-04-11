347c70f16547d164324d337ecbeb3b2c38900d70
muniryasir <muniryasir2009@gmail.com>
2023-04-11 07:53:52 +0500

    modified:   __pycache__/app.cpython-36.pyc 	modified:   app.py 	new file:   simfin_data/download/us-income-annual.zip 	new file:   simfin_data/us-income-annual.csv

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask
 app = Flask(__name__)
+import os
+
 # python3 -m flask run
 import simfin as sf
 from simfin.names import *
 
 sf.set_api_key('a3dde4ef-6674-4b65-84a3-a9ff2e7c81f2')
-sf.set_data_dir('~/simfin_data/')
+sf.set_data_dir(os.getcwd()+'/simfin_data/')
 df = sf.load_income(variant='annual', market='us')
-# @app.route("/")
-# def home():
-#     return "Hello World! I'm using Flask."
 
 @app.route("/")
+def home():
+    return "Hello World! I'm using Flask."
+
+@app.route("/stockdata")
 def stockdata():
     print(df.loc['MSFT', [REVENUE, NET_INCOME]])
-    return df.loc['MSFT', [REVENUE, NET_INCOME]]
+    return "df.loc['MSFT', [REVENUE, NET_INCOME]]"
